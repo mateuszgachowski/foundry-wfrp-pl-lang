9e46d368a913863ff3552200320fa6ba520ad1f6
Mateusz Gachowski <628992+mateuszgachowski@users.noreply.github.com>
2023-11-23 23:50:08 +0100

    feat: move lang name

--- babele-register.js
+++ babele-register.js
@@ -2,7 +2,7 @@ Hooks.once('init', () => {
     if (typeof Babele !== 'undefined') {
         Babele.get().register({
             module: 'wfrp4e-core-pl',
-            lang: 'pl-PL',
+            lang: 'pl',
             dir: 'compendium'
         });
     }
